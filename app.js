4c655fa3b762737365382db28a335e91ca6e753f
Abbas Ahmad <abbasahmad8032@gmail.com>
2025-07-05 19:57:27 +0500

    reset mongoose connection

--- app.js
+++ app.js
@@ -18,10 +18,9 @@ const orderRoutes = require("./routes/order");
 const app = express();
 
 // database
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI).then(()=> {
+    console.log("database is connected")
+})
 
 //middlewares
  app.use(morgan("dev"));
